20550f05e550b3ca6cfd4b78928f4258f8b49537
Sadeep Chathushan <chathushan43sadeep@gmail.com>
2025-07-22 10:00:45 +0530

    Initialize project structure using layered architecture

---
+++ src/main/java/com/paywastex/PaywastexApplication.java
@@ -0,0 +1,13 @@
+package com.paywastex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaywastexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaywastexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paywastex/PaywastexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paywastex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaywastexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
