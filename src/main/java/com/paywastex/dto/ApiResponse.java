682594d54f6f34ea03678766a3787ad23ad17b03
Sadeep Chathushan <chathushan43sadeep@gmail.com>
2025-07-22 12:06:42 +0530

    Api response and ReqRes Dto create

---
+++ src/main/java/com/paywastex/dto/ApiResponse.java
@@ -0,0 +1,22 @@
+package com.paywastex.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResponse <T> {
+    private LocalDateTime timestamp;
+    private String message;
+    private T data;
+    private int status;
+
+    public ApiResponse(Instant now, String m, Object data, int value) {
+    }
+}
+
