e32a99b15406899c2a8501ab5462b3f868b80dc8
mihiranwijesekara <achinthamihiran654@gmail.com>
2025-08-04 11:57:52 +0530

    Remove comment

---
+++ src/main/java/com/paywastex/dto/ZoneResponse.java
@@ -0,0 +1,5 @@
+package com.paywastex.dto;
+
+public class ZoneResponse {
+
+}
--- src/main/java/com/paywastex/dto/request/AddZoneRequest.java
+++ src/main/java/com/paywastex/dto/request/AddZoneRequest.java
@@ -10,19 +10,4 @@ public class AddZoneRequest {
     private String zoneName;
     private String description;
 
-//    public String getZoneCode() {return zoneCode;}
-//    public String getZoneName() {
-//        return zoneName;
-//    }
-//    public String getDescription() {return description;}
-//
-//    public void setZoneCode(String zoneCode) {
-//        this.zoneCode = zoneCode;
-//    }
-//    public void setZoneName(String zoneName) {
-//        this.zoneName = zoneName;
-//    }
-//    public void setDescription(String description) {
-//        this.description = description;
-//    }
 }
